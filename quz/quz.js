15d3f3cbef794a163c3d757c5886f55226ef7e66
Miao <miaoyunze@foxmail.com>
2017-08-07 20:53:02 +0800

    homework - karma test

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,16 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	if(!A){
+		//return B directly if A does not exist
+		return B;
+	}
+	var lastEleOfA = A;
+	while(lastEleOfA.tail){
+		lastEleOfA = lastEleOfA.tail;
+	}
+	lastEleOfA.tail = B;
+	return A;
 }
 
 /**
@@ -24,4 +34,33 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	if(!L){
+		return [];
+	}
+	// calculate length of L
+	var iLength = 0;
+	var iTemp = L;
+	while(iTemp){
+		iTemp = iTemp.tail;
+		iLength++;
+	}
+	if(start + len > iLength){
+		// this means the array wanted exceed the length of L
+		console.log("ERROR: Wanted length larger than we could provide");
+		return ;
+	}
+	var eleBegin = L;
+	// get begin index of arr
+	while(start !== 0){
+		eleBegin = eleBegin.tail;
+		start--;
+	}
+	var resultArr = [eleBegin.head];
+	len--;
+	while(len--){
+		// push len elements in resultArr
+		eleBegin = eleBegin.tail;
+		resultArr.push(eleBegin.head);
+	}
+	return List.list(resultArr);
 }
