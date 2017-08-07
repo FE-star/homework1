884c3da6487143c4de59d3897d1a60a37580e1d3
Cheery <838529779@qq.com>
2017-08-08 00:09:06 +0800

    提交作业
    提交作业

--- quz/quz.js
+++ quz/quz.js
@@ -1,3 +1,7 @@
+function isEmpty(obj) {
+    return obj === null || obj === undefined || obj === '';
+}
+
 /**
  * dcate
  * A list consisting of elements of A followed by the
@@ -8,7 +12,24 @@
  * @returns {List}
  */
 function dcate(A, B) {
-	/** Fill in here **/
+    if (isEmpty(A)) {
+        throw 'A不能为空';
+    }
+
+    if (isEmpty(B)) {
+        throw 'B不能为空';
+    }
+
+    // 查找A的最深层
+    var tmpA = A;
+    while (tmpA.tail != null) {
+        tmpA = tmpA.tail;
+    }
+
+    // 追加B
+    tmpA.tail = B;
+
+    return A;
 }
 
 /**
@@ -23,5 +44,38 @@ function dcate(A, B) {
  * @returns {List}
  */
 function sub(L, start, len) {
-	/** Fill in here **/
+    if (isEmpty(L)) {
+        throw '要截取的List不能为空';
+    }
+
+    if (isEmpty(start)) {
+        start = 0;
+    }
+
+    if (isEmpty(len)) {
+        len = -1;
+    }
+
+    var newArr = [];
+    var temp = { tail: L };
+    var index = -1;
+
+    do {
+        index += 1;
+        temp = temp.tail;
+
+        // 跳过start之前的item
+        if (index < start) {
+            continue;
+        }
+
+        // len越界 or 达到指定个数
+        if (isEmpty(temp) || (len != -1 && newArr.length == len)) {
+            break;
+        }
+
+        newArr.push(temp.head);
+    } while (temp != null)
+
+    return List.list(newArr);
 }
