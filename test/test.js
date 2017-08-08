710c2722c07972ab1ba7ea498099b5155aef4c4f
郑海旺 <zhenghaiwang@dangdang.com>
2017-08-08 11:36:37 +0800

    done

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@ describe('unit test for quz.js', function () {
     var A = List.list([4, 6, 7, 3, 8]),
         B = List.list([3, 2, 5, 9]),
         C = List.list([19, 8, 7, 3, 2]);
-
     describe('#dcate()', function () {
         it('should get a list consisting of elements of A followed by the elements of B', function () {
         	A.toString().should.equal('[ 4 6 7 3 8 ]');
