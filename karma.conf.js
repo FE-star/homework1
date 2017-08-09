29d4e65e632ecc681bd4d3c19ba488142b37ee71
smallbonelu <lue2048@163.com>
2017-08-09 21:03:40 +0800

    add Coveralls service

--- karma.conf.js
+++ karma.conf.js
@@ -30,14 +30,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'quz/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
@@ -67,6 +67,12 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    // optionally, configure the reporter
+    coverageReporter: {
+    type: 'lcov', // lcov or lcovonly are required for generating lcov.info files 
+    dir: 'coverage/'
+  }
   })
 }
