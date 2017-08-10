bacd29929995a162c66939e4e90bbbc126c3a672
李志鹏 <lizhipeng@lizhipengdeMac-mini.local>
2017-08-10 23:50:56 +0800

    travis-ci

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,22 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	//检测A和B是否是自定义链表List类型的实例，任意一个不是的话即抛出错误
+	if(!(A instanceof List) || !(B instanceof List)) {
+		throw new TypeError('"A"和"B"都应该是List类型实例')
+	}
+	//检测B是否为空，为空直接返回A
+	if(!B){
+		return A;
+	}
+	var temp = A;
+	//循环列表，直到找到最后一项
+	while(temp.tail !== null) {
+		temp = temp.tail
+	}
+	//找到最后一项后，把它的tail赋为B，建立链接
+	temp.tail = B;
+	return A;
 }
 
 /**
@@ -24,4 +40,22 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	if(start < 0 || len < 0) {
+		return new Error('start或len设定值超出范围');
+	}
+	var temp = L,
+		index = 0,
+		length = 0,
+		array = [];
+	//循环列表知道最后一项，同时获取新数组array
+	while(temp !== null){
+		//从设置的start开始，同时防止超过设定len
+		if(index >= start && length <= len){
+			array.push(temp.head);
+			length++;
+		}
+		index++;
+		temp = temp.tail;
+	}
+	return List.list(array);
 }
