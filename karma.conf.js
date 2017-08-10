38c1d5368e6d06ee96606f008d3e3e60d133541f
Jeffrey-zhao <zhao_shuangwu@163.com>
2017-08-10 16:26:56 +0800

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -28,19 +28,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'quz/**/*.js':['coverage']
+        
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','coverage'],
+    reporters: ['progress'],
 
-    coverageReporter:{
-        type:'html',
-        dir:'coverage/'
-    },
     // web server port
     port: 9876,
 
