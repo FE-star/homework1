fd90531f63bb159e9994e157d20c8ef3b49211b4
郑海旺 <zhenghaiwang@dangdang.com>
2017-08-10 10:23:09 +0800

    test

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'test/**/*.js': ['coverage']
+      'js/*.js':['coverage'],
+      'quz/*.js':['coverage'],
+      'test/*.js':['coverage']
     },
 
 
