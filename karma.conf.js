8b188dddddd1f1189b0d3df99967ba2e3757e037
YooHannah <1942063705@qq.com>
2017-08-13 09:59:16 +0800

    test branch and coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -34,8 +34,6 @@ module.exports = function(config) {
         'quz/*.js': ['coverage']
     },
 
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
