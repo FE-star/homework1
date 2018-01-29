8b41f01d9935ad1db89ab5304cf682d3a83d045e
JaffarL <lyzandwow@163.com>
2018-01-29 22:57:41 +0800

    完成

--- quz/quz.js
+++ quz/quz.js
@@ -1,3 +1,4 @@
+
 /**
  * dcate
  * A list consisting of elements of A followed by the
@@ -9,6 +10,11 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	let temp = A.tail;
+	for(;temp.tail!=null;temp=temp.tail){
+	}
+	temp.tail = B;
+	return A
 }
 
 /**
@@ -24,4 +30,19 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+
+	let templist = [] 
+	let temp = L.tail;
+	for(let i = 1;i<start;i++){
+		temp = temp.tail;
+	}
+	for(let i =0;i<len;i++){
+		templist.push(temp.head);
+		temp = temp.tail;
+	}
+	let subList = List.list(templist);
+	return subList
 }
+
+
+
