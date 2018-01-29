67eec9ca72938b9ebf7d11bcb8eeb40e64f2eff5
null-br <wanglu1@yy.com>
2018-01-29 18:09:11 +0800

    test

--- quz/quz.js
+++ quz/quz.js
@@ -28,13 +28,8 @@ function dcate(A, B) {
 
 function sub(L, start, len) {
 	/** Fill in here **/
-	let resArr = [],
-		end = start + len;
-	for (A = L, i = 0; A !== null; A = A.tail, i++) {
-		if (i >= start)
-			resArr.push(A.head);
-		if (i > end || A.tail === null)
-			break;
-	}
-	return L.constructor.list(resArr);
+	if (len === 0) return null
+	var startIndex = L.indexOf(start)
+	if (startIndex === 0) return L
+	return L.slice(startIndex, len)
 }
