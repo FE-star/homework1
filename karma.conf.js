3d2538e372abded4fd115cccb68155b121241a21
dingxiaofei <dingxiaofei@didichuxing.com>
2018-01-29 20:49:25 +0800

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -58,9 +58,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome_travis_ci'],
 
-
+    customLaunchers: {
+        Chrome_travis_ci:{
+            base: 'Chrome',
+            flags:["--no-sandbox"]
+        }
+    },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
