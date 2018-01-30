93aa09b130d579e2257a45c14b46523c7ccbe08c
hookehuyr <hookehuyr@gmail.com>
2018-01-30 11:18:33 +0800

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.js': ['coverage']
+      // 'test/*.js': ['coverage']
     },
 
 
@@ -42,8 +42,8 @@ module.exports = function(config) {
 
     // optionally, configure the reporter
     coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
+      // type : 'html',
+      // dir : 'coverage/'
     },
 
 
