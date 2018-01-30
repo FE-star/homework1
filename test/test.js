df29dbc2bd69af409c73ae65d0e593391359c7b5
wangyuanli <yuanli.wang@xiaozao.org>
2018-01-30 15:40:06 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ describe('unit test for quz.js', function () {
 
     var A = List.list([4, 6, 7, 3, 8]),
         B = List.list([3, 2, 5, 9]),
-        C = List.list([19, 8, 7, 3, 2]);
+        C = List.list([19, 8, 7, 6, 7]);
 
     describe('#dcate()', function () {
         it('should get a list consisting of elements of A followed by the elements of B', function () {
-        	A.toString().should.equal('[ 4 6 7 3 8 ]');
+            A.toString().should.equal('[ 4 6 7 3 8 ]');
             dcate(A, B).toString().should.equal('[ 4 6 7 3 8 3 2 5 9 ]');
             A.toString().should.not.equal('[ 4 6 7 3 8 ]');
         });
@@ -14,10 +14,12 @@ describe('unit test for quz.js', function () {
 
     describe('#sub()', function () {
         it('should get the sublist consisting of LEN items from list L', function () {
-        	C.toString().should.equal('[ 19 8 7 3 2 ]');
-            sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
-            C.toString().should.equal('[ 19 8 7 3 2 ]');
+            C.toString().should.equal('[ 19 8 7 6 7 ]');
+            sub(C, 3, 2).toString().should.equal('[ 6 7 ]');
+            sub(C, 1, 1).toString().should.equal('[ 8 ]');
+            sub(C, 0, 1).toString().should.equal('[ 19 ]');
+            sub(C, 0, 5).toString().should.equal('[ 19 8 7 6 7 ]');
+            C.toString().should.equal('[ 19 8 7 6 7 ]');
         });
     });
-
 });
