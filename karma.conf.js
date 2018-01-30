864745d00b0de7d89bbf6ec346a1e2ef810550e7
Mr <lting1010820@gmaail.com>
2018-01-31 00:01:02 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -12,9 +12,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      "https://cdn.bootcss.com/jquery/2.2.4/jquery.js",
-      "node_modules/should/should.js",
-      "test/**.js"
+      'node_modules/should/should.js',
+      'js/*.js',
+      'quz/*.js',
+      'test/*.js'
     ],
 
     // list of files / patterns to exclude
