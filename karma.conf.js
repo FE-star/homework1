93962d778b2d924bfcbad44658c58a24c4e54e5a
shen <357789504@qq.com>
2018-01-31 22:16:27 +0800

    del():删除firefox

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['chrome_without_security','Firefox'],
+    browsers: ['chrome_without_security'],
     customLaunchers: {
       chrome_without_security: {
         base: 'Chrome',
