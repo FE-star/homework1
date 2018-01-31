50998c41a277ed39b88f6bd86ea77bb077105e9b
PennyOrAmy <penny_kay@126.com>
2018-01-31 16:07:24 +0800

    trigger coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -23,12 +23,16 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+      'quz/*.js': 'coverage'
+    },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    coverageReporter: {
+      type: 'lcov',
+      dir: './coverage/'
+    },
+
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
