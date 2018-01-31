959bddce36cb9744f5cda62709e22af27c500b84
null-br <wanglu1@yy.com>
2018-01-31 14:16:43 +0800

    up

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,11 @@
-// Karma configuration
-// Generated on Thu Aug 10 2017 11:22:38 GMT+0800 (中国标准时间)
-
 module.exports = function (config) {
-  config.set({
-
+  var configuration = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-
+    frameworks: ["mocha"],
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,46 +15,50 @@ module.exports = function (config) {
       'test/*.js'
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
+    // list of files / patterns to exclude
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'quz/*.js': ['coverage']
     },
-
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: 'coverage/' },
+        { type: 'text-summary' }
+      ]
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha', 'coverage'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
 
+    // travis cli 使用 chrome 测试
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: "Chrome",
+        flags: ["--no-sandbox"]
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -68,13 +66,11 @@ module.exports = function (config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    coverageReporter: {
-      reporters: [
-        { type: 'lcov', dir: 'coverage/' },
-        { type: 'text-summary' }
-      ]
-    }
-  })
-}
+    concurrency: Infinity
+  };
+  // https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
+  if (process.env.TRAVIS) {
+    configuration.browsers = ["Chrome_travis_ci"];
+  }
+  config.set(configuration);
+};
