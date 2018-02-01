b578b3d6547b1707ea57a867017d2b462dbe9adf
Tiny <453616448@qq.com>
2018-02-01 10:29:09 +0800

    update

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,15 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	var node = A;
+	while(node) {
+		if(!node.tail) {
+			node.tail = B;
+			break;
+		}
+		node = node.tail;
+	}
+	return A;
 }
 
 /**
@@ -24,4 +33,20 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	var idx = 0;
+	var length = 0;
+	var node = L;
+	var new_node = null;
+	while(node) {
+		if(idx >= start && length < len) {
+			var list = new List();
+			list.head = node.head
+			if(!new_node) new_node = list;
+			else new_node.tail = list;	
+			length++;
+		}
+		node = node.tail;
+		idx++;
+	}
+	return new_node;
 }
