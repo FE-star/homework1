2daabfcf4c36446a410925d98b0bafbcc3a3c623
Tiny <453616448@qq.com>
2018-02-01 19:00:59 +0800

    添加coverage

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        "**/lib/*js": "coverage"
+        "js/*.js",
+        "quz/*.js",
+        "test/*.js"
     },
 
 
