f9eb86e8436d9d81a167ca6a2eda5b5287368802
Tiny <453616448@qq.com>
2018-02-01 16:11:37 +0800

    修改conf的browser

--- karma.conf.js
+++ karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'FirefoxHeadless', 'Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Firefox', 'Chrome'],
 
     // you can define custom flags
     customLaunchers: {
