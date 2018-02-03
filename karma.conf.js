7779bada83e097fdb50ee17a59482dc57b11561c
劳铱婷 <eating_miao@163.com>
2018-02-04 00:46:39 +0800

    connect ci

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
 
     // Continuous Integration mode
@@ -69,4 +74,9 @@ module.exports = function(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   })
+  
+  if(process.env.TRAVIS) {
+    config.browsers = ['Chrome_travis_ci'];
+  }
+
 }
