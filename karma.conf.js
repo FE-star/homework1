eed35c7adbf46537232d972642d4632eee9c0f4a
dingxiaofei <dingxiaofei@didichuxing.com>
2018-02-04 14:51:08 +0800

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage','coveralls'],
 
-    coverageReportr: {
+    coverageReporter: {
         type: 'lcov',
         dir: 'coverage'
     },
