bc4557d1569b0980d5c722cf1777ea585fe5c14b
dingxiaofei <dingxiaofei@didichuxing.com>
2018-02-04 14:29:56 +0800

    upate karma

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome','Chrome_travis_ci'],
+    browsers: ['Chrome_travis_ci'],
     customLaunchers: {
           Chrome_travis_ci: {
             base: "Chrome",
