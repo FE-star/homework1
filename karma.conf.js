c986128f8ed5ddb3a38d4f8ffe0d36401ca807f2
Mcyfeng <35359402+Mcyfeng@users.noreply.github.com>
2018-02-09 00:25:05 +0800

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Wed Jan 31 2018 22:39:44 GMT+0800 (中国标准时间)
+// Generated on Fri Aug 04 2017 20:53:38 GMT+0800 (CST)
 
 module.exports = function(config) {
   config.set({
@@ -14,12 +14,13 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-      files: [
-          'node_modules/should/should.js',
-          'js/*.js',
-          'quz/*.js',
-          'test/*.js'
-      ],
+    files: [
+      'node_modules/should/should.js',
+      'js/*.js',
+      'quz/*.js',
+      'test/*.js'
+    ],
+
 
     // list of files to exclude
     exclude: [
@@ -29,14 +30,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'quz/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage', 'coveralls'],
 
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
@@ -57,12 +64,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: process.env.TRAVIS,
 
     // Concurrency level
     // how many browser should be started simultaneous
