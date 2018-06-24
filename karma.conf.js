26ac220529cec809af4b4250fb3a0b7565811a28
snowfighting <songxj0130@126.com>
2018-06-24 18:35:16 +0800

    fix(): bug

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
@@ -63,7 +63,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
