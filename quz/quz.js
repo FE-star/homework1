3a84485446b35fadf568fa3941fde16f22b409d1
houjunjie <376969465@qq.com>
2018-11-04 21:25:09 +0800

    done

--- quz/quz.js
+++ quz/quz.js
@@ -1,3 +1,4 @@
+
 /**
  * dcate
  * A list consisting of elements of A followed by the
@@ -9,8 +10,16 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	let temp = A.tail;
+	while(temp){
+		if(!temp.tail){
+			temp.tail = B
+			break;
+		}
+		temp = temp.tail;
+	}
+	return A
 }
-
 /**
  * sub
  * The sublist consisting of LEN items from list L,
@@ -24,4 +33,19 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	let temp = L.tail, num = 0, res = [];
+	while (temp) {
+		if(temp.head === start || res.length>0 && res.length < len){
+			res.push(temp.head);
+		}
+		temp = temp.tail;
+	}
+	if(res.length === 0) {
+		throw new Error(`找不到开始值：${start}`);
+	}
+
+	if(res.length < len) {
+		throw new Error(`没有足够的值`);
+	}
+	return List.list(res);
 }
