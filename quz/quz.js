d230b3d40e20d4cd4a2065d12faa73df925f02c6
朴野归踪 <2472906920@qq.com>
2018-11-05 15:38:40 +0800

    本地test通过

--- quz/quz.js
+++ quz/quz.js
@@ -8,7 +8,20 @@
  * @returns {List}
  */
 function dcate(A, B) {
-	/** Fill in here **/
+  /** 判断参数的原型，含非List参数时，不予计算 **/
+  if (A.__proto__ != List.prototype || B.__proto__ != List.prototype) {
+    return A.__proto__ != List.prototype ? (
+      B.__proto__ != List.prototype ? null : B
+    ) : A ;
+  } else {
+    /** 遍历出A的tail，将B链上去（A可以修改），然后返回A */
+    let target = A;
+    while(target.tail != null){
+      target = target.tail;
+    };
+    target.tail = B;
+    return A;
+  }
 }
 
 /**
@@ -23,5 +36,29 @@ function dcate(A, B) {
  * @returns {List}
  */
 function sub(L, start, len) {
-	/** Fill in here **/
+  if(len === 0)
+    return "[ ]";
+
+  let target = L,
+      begin = start - 1,
+      end = start + len - 1,
+      index = 0;
+      result = new List(),
+      pointer = result;
+
+  while(target){
+    if(begin <= index && index <= end){
+      let temp = new List(target.head);
+      pointer.tail = temp;
+      /** 复制最新List的引用 */
+      pointer = temp;
+    }
+    index++;
+    target = target.tail;
+  };
+  
+  if(result.tail === null)
+    throw new RangeError("the desired items don't exist.");
+
+  return result.tail.tail;
 }
--- test/test.js
+++ test/test.js
@@ -4,40 +4,38 @@ describe('unit test for quz.js', function () {
         B = List.list([3, 2, 5, 9]),
         C = List.list([19, 8, 7, 3, 2]);
 
+
+    describe('#list()', function () {
+        it('新建list时无参数,返回null', function () {
+            should(List.list([])).be.exactly(null);
+        });
+    });
+
     describe('#dcate()', function () {
         it('should get a list consisting of elements of A followed by the elements of B', function () {
-        	A.toString().should.equal('[ 4 6 7 3 8 ]');
-            // dcate(A, B).toString().should.equal('[ 4 6 7 3 8 3 2 5 9 ]');
-            // A.toString().should.not.equal('[ 4 6 7 3 8 ]');
+            A.toString().should.equal('[ 4 6 7 3 8 ]');
+              dcate(A, B).toString().should.equal('[ 4 6 7 3 8 3 2 5 9 ]');
+              A.toString().should.not.equal('[ 4 6 7 3 8 ]');
+        });
+        it('含非List参数，则直接返回参数中的List，若都无则返回空', function () {
+            dcate([1, 2, 3], B).toString().should.equal('[ 3 2 5 9 ]');
+            dcate(C, [1, 2, 3]).toString().should.equal('[ 19 8 7 3 2 ]');
+            should(dcate([1, 2], [3, 4])).be.exactly(null);
         });
     });
 
     describe('#sub()', function () {
         it('should get the sublist consisting of LEN items from list L', function () {
-        	C.toString().should.equal('[ 19 8 7 3 2 ]');
-            // sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
-            // C.toString().should.equal('[ 19 8 7 3 2 ]');
+            C.toString().should.equal('[ 19 8 7 3 2 ]');
+              sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
+              C.toString().should.equal('[ 19 8 7 3 2 ]');
+        });
+        it('截取List越界时：(1)begin不越界，end越界，返回有效的长度', function () {
+            sub(C, 3, 8).toString().should.equal('[ 3 2 ]');
+        });
+        it('截取List越界时：(2)begin越界，抛错RangeError', function () {
+            should.throws(()=>{sub(C, 8, 2)}, /^RangeError: the desired items don't exist.$/)
         });
     });
 
 });
-
-
-/**
- * List.list([4, ...])
- * 将一个数组展开成一个链表
- * let foo = {
-        head: 0,
-        tail: {
-            head: 4,
-            tail: ...
-            }
-        }
-    }
- */
-
-/**
- * List.toString()
- * 返回List的字符串格式
- * [ 4, 6, ..., ]
- */
