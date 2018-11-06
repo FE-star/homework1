6d272559076b5ad83d050d8a7defb5b8398cbedd
RikaXia <tanchunxia@gmai.com>
2018-11-06 21:37:44 +0800

    完成第一题

--- quz/quz.js
+++ quz/quz.js
@@ -8,7 +8,19 @@
  * @returns {List}
  */
 function dcate(A, B) {
-	/** Fill in here **/
+  /** Fill in here **/
+  let pointer = A;
+  while (pointer) {
+    if (pointer.tail === null) {
+      // 当遍历到最后一个pointer.tail为null,则将整个B赋值给pointer.tail
+      pointer.tail = B;
+      // 结束遍历
+      break;
+    }
+    // pointer指向a.tail,继续往深一层查找，此时pointer指向a.tail.tail
+    pointer = pointer.tail;
+  }
+  return A;
 }
 
 /**
@@ -23,5 +35,14 @@ function dcate(A, B) {
  * @returns {List}
  */
 function sub(L, start, len) {
-	/** Fill in here **/
+  /** Fill in here **/
+  let temp = L;
+  for (let i = 0; i < start + len - 1; i++) {
+    if (i === start) {
+      L = temp;
+    }
+    temp = temp.tail;
+  }
+  temp.tail = null;
+  return L;
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,24 @@
 describe('unit test for quz.js', function () {
 
-    var A = List.list([4, 6, 7, 3, 8]),
-        B = List.list([3, 2, 5, 9]),
-        C = List.list([19, 8, 7, 3, 2]);
+  var A = List.list([4, 6, 7, 3, 8, 0]),
+    B = List.list([3, 2, 5, 9]),
+    C = List.list([19, 8, 7, 3, 2]);
 
-    describe('#dcate()', function () {
-        it('should get a list consisting of elements of A followed by the elements of B', function () {
-        	A.toString().should.equal('[ 4 6 7 3 8 ]');
-            dcate(A, B).toString().should.equal('[ 4 6 7 3 8 3 2 5 9 ]');
-            A.toString().should.not.equal('[ 4 6 7 3 8 ]');
-        });
+  describe('#dcate()', function () {
+    it('should get a list consisting of elements of A followed by the elements of B', function () {
+      A.toString().should.equal('[ 4 6 7 3 8 0 ]');
+      dcate(A, B).toString().should.equal('[ 4 6 7 3 8 0 3 2 5 9 ]');
+      A.toString().should.not.equal('[ 4 6 7 3 8 0 ]');
     });
+  });
 
-    describe('#sub()', function () {
-        it('should get the sublist consisting of LEN items from list L', function () {
-        	C.toString().should.equal('[ 19 8 7 3 2 ]');
-            sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
-            C.toString().should.equal('[ 19 8 7 3 2 ]');
-        });
+  describe('#sub()', function () {
+    it('should get the sublist consisting of LEN items from list L', function () {
+      C.toString().should.equal('[ 19 8 7 3 2 ]');
+      sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
+      C.toString().should.equal('[ 19 8 7 3 2 ]');
+      // sub(C, 3,2)
     });
+  });
 
 });
