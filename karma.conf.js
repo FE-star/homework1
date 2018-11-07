41d5e7d90ff99c2db1311fe2bbd2db08f5163404
linjinxuan <jinxuan_lin@kingdee.com>
2018-11-07 14:39:46 +0800

    modify travis to start firefox browsers

--- karma.conf.js
+++ karma.conf.js
@@ -45,14 +45,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Firefox'],
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
