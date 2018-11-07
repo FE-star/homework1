70293551928be66ee592ab978cfb616551f47521
GangYu HU <904829564@qq.com>
2018-11-08 00:18:00 +0800

    补充travis

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['chrome_without_security'],
+    customLaunchers: {
+      chrome_without_security: {
+        base: 'Chrome'
+      }
+    },
+
 
 
     // Continuous Integration mode
