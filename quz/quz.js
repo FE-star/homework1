ca2aa3f39f3c393f77eb9e88746626fa4d586ddc
mydeer <597584896@qq.com>
2019-03-17 22:05:52 +0800

    fix

--- quz/quz.js
+++ quz/quz.js
@@ -8,8 +8,17 @@
  * @returns {List}
  */
 function dcate(A, B) {
-	/** Fill in here **/
-	// hello
+	// 记录下A的内存地址
+	let res = A
+	// 一直循环，直到找到tail为null停止
+	while(res.tail) {
+		// 一层一层地查找tail对象
+		res = res.tail
+	}
+	// 找到的最终tail，地址引向B的地址，连接上list B
+	res.tail = B
+	// 不能返回res，因为此时res已经改变了，指向的是初始A的tail为null的地址
+	return A
 }
 
 /**
