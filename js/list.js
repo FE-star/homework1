9ef1729ce602099ef08c7b9ed27b4e53abc69099
wuxingping <41353266+wuxingping@users.noreply.github.com>
2019-03-18 23:26:38 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,3 +1,34 @@
 /**
- * Created by admin on 2019/3/18.
+ * List
+ * @author donaldyang
  */
+
+function List(head, tail) {
+	this.head = head || 0;
+	this.tail = tail || null;
+}
+
+// Returns a new List containing the array.
+List.list = function (array) {
+	var sentinel = new List(),
+		len = array.length,
+		p, i;
+
+	p = sentinel;
+	for (i = 0; i < len; i++) {
+		p.tail = new List(array[i]);
+		p = p.tail;
+	}
+	return sentinel.tail;
+}
+
+// Returns a readable String for THIS.
+List.prototype.toString = function () {
+	var res = '', L;
+	res += '[';
+	for (L = this; L !== null; L = L.tail) {
+		res = res + ' ' + L.head;
+	}
+	res += ' ]';
+	return res;
+};
