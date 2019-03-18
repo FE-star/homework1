5a41685f4dd2af6d9dfb169e255aeccbb7607fe3
wuxingping <676255049@qq.com>
2019-03-18 22:12:55 +0800

    吴杏平提交第一周作业

---
+++ quz/quz.js
@@ -0,0 +1,50 @@
+/**
+ * dcate
+ * A list consisting of elements of A followed by the
+ * elements of B. May modify items of A.
+ * Don't use 'new'
+ * @param {List} A
+ * @param {List} B
+ * @returns {List}
+ */
+function dcate(A, B) {
+	/** Fill in here **/
+    var L,m;
+    m=A;
+    for (L = A; L !== !null; L = L.tail) {
+        if(L.tail==null){
+            L.tail = B;
+            return m;
+        }
+
+    }
+}
+
+/**
+ * sub
+ * The sublist consisting of LEN items from list L,
+ * beginning with item #START (where the first item is #0).
+ * Does not modify the original list elements.
+ * it is an error if the desired items don't exist.
+ * @param {List} L
+ * @param {Number} start
+ * @param {Number} len
+ * @returns {List}
+ */
+function sub(L, start, len) {
+	/** Fill in here **/
+    var M,res = '',index = 0,count = 0;
+    res += '[';
+    for (M = L; M !=null; M = M.tail) {
+        if(index>=start){
+            count++;
+            res = res + ' ' + M.head;
+            if(count==len){
+                break;
+            }
+        };
+        index++;
+    }
+    res += ' ]';
+    return res;
+}
