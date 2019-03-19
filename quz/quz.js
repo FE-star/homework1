db9190e6d115c33d2a65d6438f7892419f94717e
lei <1402328968@qq.com>
2019-03-19 19:28:26 +0800

    dcate and sub

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,13 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	var L;
+	for(L = A; L !== null;L = L.tail ){
+		if(L.tail == null){
+			L.tail = B;
+		}
+	}
+	return A;
 }
 
 /**
@@ -22,6 +29,24 @@ function dcate(A, B) {
  * @param {Number} len
  * @returns {List}
  */
-function sub(L, start, len) {
+function sub(A,start,len) {
 	/** Fill in here **/
+	function sub(A,start,len){
+		var L = A,S;
+		for(var i=0;i<start;i++){
+			L = L.tail;
+		}
+		S = L;
+		for(var j=0;j<len;j++){
+			
+			if(S.tail == null) return L;	
+			if(j == len-1){
+				S.tail = null;
+				return L;
+			}
+			S = S.tail;
+		}
+		
+		return L;
+	}
 }
