cd1903d2b6ddbebe6146aa6b987eaca9ee4e88b8
Grizz <panan521@qq.com>
2019-03-20 15:22:31 +0800

    提交作业

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Fri Aug 04 2017 20:53:38 GMT+0800 (CST)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -58,12 +58,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
--- quz/quz.js
+++ quz/quz.js
@@ -3,12 +3,19 @@
  * A list consisting of elements of A followed by the
  * elements of B. May modify items of A.
  * Don't use 'new'
+ * 一个A在前，B在后的列表，允许修改A，不能使用new
  * @param {List} A
  * @param {List} B
  * @returns {List}
  */
 function dcate(A, B) {
-	/** Fill in here **/
+	var tempList;
+	tempList = A
+	while (tempList.tail !== null) {
+		tempList = tempList.tail
+	}
+	tempList.tail = B
+	return A
 }
 
 /**
@@ -17,11 +24,36 @@ function dcate(A, B) {
  * beginning with item #START (where the first item is #0).
  * Does not modify the original list elements.
  * it is an error if the desired items don't exist.
+ * 子表是母表的一部分，长度是参数len
+ * start参数是开始下标
+ * 不允许修改母表
+ * 如果长度超出抛出错误
  * @param {List} L
  * @param {Number} start
  * @param {Number} len
  * @returns {List}
  */
 function sub(L, start, len) {
-	/** Fill in here **/
+	let end = start + len
+	var paramIndex,
+		result,
+		resultIndex;
+	var i = 0,
+		paramIndex = L;
+	while (i !== start) {
+		i++
+		paramIndex = paramIndex.tail//找到当前层级，对应当前下标
+	}
+	result = new List(paramIndex.head);
+	resultIndex = result;
+	i++//初始化要返回的list的第一个
+	for (paramIndex = paramIndex.tail; i <= end; paramIndex = paramIndex.tail, i++) {
+		if (i < end) {
+			resultIndex.tail = new List(paramIndex.head);
+			resultIndex = resultIndex.tail;
+		} else if (i >= end) {
+			break;
+		}
+	}
+	return result;
 }
