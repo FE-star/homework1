5141878b26d8a1bf9ceab8e4724d2e38257708fe
蔡胜龙 <1187023819@qq.com>
2019-03-20 21:58:20 +0800

    [去掉chromeLauncher配2]

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Chrome_without_security'],
+    browsers: ['Chrome'], //, 'Chrome_without_security'
 
     /* customLaunchers: {
       Chrome_without_security: {
