e0a7e9b55ed9d3fa39f7ea46a2f46badb825df6d
蔡胜龙 <1187023819@qq.com>
2019-03-20 21:45:00 +0800

    调整

--- test/test.js
+++ test/test.js
@@ -6,17 +6,17 @@ describe('unit test for quz.js', function () {
 
     describe('#dcate()', function () {
         it('should get a list consisting of elements of A followed by the elements of B', function () {
-        	// A.toString().should.equal('[ 4 6 7 3 8 ]');
+        	A.toString().should.equal('[ 4 6 7 3 8 ]');
             dcate(A, B).toString().should.equal('[ 4 6 7 3 8 3 2 5 9 ]');
-            // A.toString().should.not.equal('[ 4 6 7 3 8 ]');
+            A.toString().should.not.equal('[ 4 6 7 3 8 ]');
         });
     });
 
     describe('#sub()', function () {
         it('should get the sublist consisting of LEN items from list L', function () {
-        	// C.toString().should.equal('[ 19 8 7 3 2 ]');
+        	C.toString().should.equal('[ 19 8 7 3 2 ]');
             sub(C, 3, 2).toString().should.equal('[ 3 2 ]');
-            // C.toString().should.equal('[ 19 8 7 3 2 ]');
+            C.toString().should.equal('[ 19 8 7 3 2 ]');
         });
     });
 
