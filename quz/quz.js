532de5afebe76a0457b135d0259757272b2089e0
YvonneYang <yvongyang@163.com>
2019-03-23 14:08:39 +0800

    Finish

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,15 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	var temp = A;
+
+	while(A.tail !== null) {
+		A = A.tail;
+	}
+
+	A.tail = B;
+
+	return temp;
 }
 
 /**
@@ -24,4 +33,38 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	var temp = L;
+	var i = 0;
+
+	if (len < 0 || start < 0) {
+		throw('Start or length should\'t less then 0');
+	}
+
+	while (i < start) {
+		if (L === null) {
+			throw('start is larger than L\'s length');
+		}
+
+		L = L.tail;
+		i++;
+	};
+
+	temp = L;
+
+	if (len === 0) {
+		return {};
+	}
+
+	var j = 1;
+	while(L !== null) {
+		if (j === len) {
+			L.tail = null;
+			break;
+		}
+
+		L = L.tail;
+		j++;
+	}
+
+	return temp;
 }
