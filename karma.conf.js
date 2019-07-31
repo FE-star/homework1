3247905b1cc354701dc5d48f7bc5c4f71c832cfc
zx <acczhuang@163.com>
2019-07-31 18:09:11 +0800

    测试通过

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Fri Aug 04 2017 20:53:38 GMT+0800 (CST)
+// Generated on Wed Jul 31 2019 18:06:08 GMT+0800 (GMT+08:00)
 
 module.exports = function(config) {
   config.set({
@@ -15,10 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/should/should.js',
-      'js/*.js',
-      'quz/*.js',
-      'test/*.js'
     ],
 
 
@@ -58,19 +54,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome_without_security'],
-
-    customLaunchers: {
-      Chrome_without_security: {
-        base: 'Chrome',
-        flags: ['--disable-web-security']
-      }
-    },
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
