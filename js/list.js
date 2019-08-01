69231b988eeb2d78792276c95daa8db18778784f
cai <cairenjie@hzed.com>
2019-08-01 18:14:27 +0800

    update

--- js/list.js
+++ js/list.js
@@ -4,31 +4,31 @@
  */
 
 function List(head, tail) {
-	this.head = head || 0;
-	this.tail = tail || null;
+  this.head = head || 0;
+  this.tail = tail || null;
 }
 
 // Returns a new List containing the array.
 List.list = function (array) {
-	var sentinel = new List(),
-		len = array.length,
-		p, i;
+  var sentinel = new List(),
+    len = array.length,
+    p, i;
 
-	p = sentinel;
-	for (i = 0; i < len; i++) {
-		p.tail = new List(array[i]);
-		p = p.tail;
-	}
-	return sentinel.tail;
+  p = sentinel;
+  for (i = 0; i < len; i++) {
+    p.tail = new List(array[i]);
+    p = p.tail;
+  }
+  return sentinel.tail;
 }
 
 // Returns a readable String for THIS.
 List.prototype.toString = function () {
-	var res = '', L;
-	res += '[';
-	for (L = this; L !== null; L = L.tail) {
-		res = res + ' ' + L.head;
-	}
-	res += ' ]';
-	return res;
+  var res = '', L;
+  res += '[';
+  for (L = this; L !== null; L = L.tail) {
+    res = res + ' ' + L.head;
+  }
+  res += ' ]';
+  return res;
 };
