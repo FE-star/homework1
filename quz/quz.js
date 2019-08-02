324064cd1d15a5ad1c924ac54e9eba8b314f66af
DIR\meixiu.yang <meixiu.yang@accenture.com>
2019-08-02 23:02:51 +0800

    done

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,16 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	let _list = A.tail;
+	while (_list) {
+		if (!_list.tail) {
+			_list.tail = B
+			break;
+		}
+		_list = _list.tail;
+	}
+
+	return A;
 }
 
 /**
@@ -24,4 +34,38 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	if (start < 0) {
+		throw new Error(`start 不能少于0`);
+	}
+	if (len <= 0) {
+		throw new Error(`len 不能少于等于0`);
+	}
+
+	let _list = new List();
+	let _len = 0;
+	let _arr = [];
+	for (let i = 0; L.tail !== null; i++) {
+		if (i !== 0) {
+			L = L.tail;
+		}
+		if (i === start) {
+			_list = L;
+			while (_list) {
+				if (_len < len) {
+					_arr.push(_list.head);
+				}
+				_list = _list.tail;
+				_len++;
+			}
+			break;
+		}
+	}
+	if (_arr.length === 0) {
+		throw new Error(`找不到起始值`);
+	}
+	if (_arr.length < len) {
+		throw new Error(`新组成的list长度不足`);
+	}
+
+	return List.list(_arr);
 }
